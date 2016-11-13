c068fa15f5583704c30bdd9b0bc7d8fde234e837
James <jameskr97@gmail.com>
2016-11-13 17:08:24 -0500

    File formatted. Refactored menu code.

--- src/xyz/jameskr/fm/schedule/FlightSchedule.java
+++ src/xyz/jameskr/fm/schedule/FlightSchedule.java
@@ -16,18 +16,18 @@ public class FlightSchedule {
     private ArrayList<Airline> airlines;
     private HashMap<String, Flight> flights;
 
-    public FlightSchedule(){
+    public FlightSchedule() {
         airlines = new ArrayList<>();
         flights = new HashMap<>();
         airlines.add(0, new Airline("Delta Air", "DA"));
         airlines.add(1, new Airline("James Air", "JA"));
     }
 
-    public void clearSchedule(){
+    public void clearSchedule() {
 
         System.out.print("Are you sure you want to delete all flights?\n↦ ");
         String response = Util.getInput();
-        if (response.equals("y") || response.equals("yes")){
+        if (response.equals("y") || response.equals("yes")) {
             flights.clear();
             System.out.println("Schedule cleared.");
         } else {
@@ -36,33 +36,26 @@ public class FlightSchedule {
 
     }
 
-    public void manageAirlines(){
+
+    // AIRLINE MANAGEMENT
+    public void manageAirlines() {
         ConsoleMenu menu = new ConsoleMenu("Manage airline menu");
         menu.addOption(0, "List Airlines");
         menu.addOption(1, "Add Airline");
         menu.addOption(2, "Remove Airline");
         menu.addOption(3, "Return to main menu");
 
-
-
-        while(true){
+        while (true) {
             menu.printOptions();
             int res = menu.getResponse();
-
             if (res == 0) {
-                if (airlines.size() == 0){
-                    System.out.println("There are no airlines.");
-                } else {
-                    System.out.println("Airline\t\t\tCode\tAircraft");
-                    System.out.println("=====================================");
-                    for (Airline a : airlines)
-                        System.out.printf("%s\t\t%s\t\t%s\n", a.getName(), a.getAirlineCode(), a.getAircraft().getName());
-                }
-            } else if (res == 1){
+                this.listAirlines();
+            } else if (res == 1) {
                 this.addAirline();
-            } else if (res == 2){
+            } else if (res == 2) {
                 System.out.println("Option not implemented yet.");
-            } else if(res == 3){
+            } else if (res == 3) {
+                System.out.println("Returning...");
                 break;
             }
         }
@@ -70,13 +63,24 @@ public class FlightSchedule {
 
     }
 
-    private void addAirline(){
+    private void listAirlines() {
+        if (airlines.size() == 0) {
+            System.out.println("There are no airlines.");
+        } else {
+            System.out.println("Airline\t\t\tCode\tAircraft");
+            System.out.println("=====================================");
+            for (Airline a : airlines)
+                System.out.printf("%s\t\t%s\t\t%s\n", a.getName(), a.getAirlineCode(), a.getAircraft().getName());
+        }
+    }
+
+    private void addAirline() {
         // Get airline name and code...
         Interrogator asker = new Interrogator();
-        asker.addQuestion(0, "Enter Airline Name: ", "Airline name already exists.",response -> {
+        asker.addQuestion(0, "Enter Airline Name: ", "Airline name already exists.", response -> {
             boolean canProceed = true; // As in "Can Proceed to next question."
-            for (Airline a : airlines){
-                if (a.getName().equals(response)){
+            for (Airline a : airlines) {
+                if (a.getName().equals(response)) {
                     canProceed = false;
                     break;
                 }
@@ -86,8 +90,8 @@ public class FlightSchedule {
 
         asker.addQuestion(1, "Enter Airline code: ", "Airline code already exists.", response -> {
             boolean canProceed = true; // As in "Can Proceed to next question."
-            for (Airline a : airlines){
-                if (a.getAirlineCode().equals(response)){
+            for (Airline a : airlines) {
+                if (a.getAirlineCode().equals(response)) {
                     canProceed = false;
                     break;
                 }
@@ -96,7 +100,7 @@ public class FlightSchedule {
         });
 
         String[] response = asker.ask();
-        if(response == null){ // Will only return null if user responded with "no" to a try again prompt
+        if (response == null) { // Will only return null if user responded with "no" to a try again prompt
             System.out.println("Operation canceled.");
             return;
         }
