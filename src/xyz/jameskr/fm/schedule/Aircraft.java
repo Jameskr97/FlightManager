5054f68035003884eace625d243890ca723842b3
James <jameskr97@gmail.com>
2016-11-13 17:03:15 -0500

    Refactored Aircraft. Added default aircraft.

--- src/xyz/jameskr/fm/schedule/Aircraft.java
+++ src/xyz/jameskr/fm/schedule/Aircraft.java
@@ -5,8 +5,25 @@ package xyz.jameskr.fm.schedule;
  * @date 11/12/16
  */
 public class Aircraft {
+
+    public static Aircraft DEFAULT_AIRCRAFT = new Aircraft();
+
     private String model;
-    private int capacity;
-    private int cargoCapacity;
-    private int passengerCapacity;
+    private int cargoCapacity; //square feet
+    private int passengerCapacity; // people
+
+    public Aircraft(){
+        this.model = "Boeing 777-9";
+        this.cargoCapacity = 5330;
+        this.passengerCapacity =  400;
+    }
+
+    public String getName() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Aircraft: %s\nCargo Capacity: %d\nMax Passengers: %d", model, cargoCapacity, passengerCapacity);
+    }
 }
