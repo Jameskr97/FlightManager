e2ee9c976733201f114640b3308fc09647f29acd
James <jameskr97@gmail.com>
2016-11-14 02:13:12 -0500

    Added descriptions to enums

--- src/xyz/jameskr/fm/schedule/enums/FlightStatus.java
+++ src/xyz/jameskr/fm/schedule/enums/FlightStatus.java
@@ -1,22 +1,31 @@
 package xyz.jameskr.fm.schedule.enums;
 
 /**
+ * Enum to easy interface with flight status required datatype of char.
+ *
  * @author James 
  * @date 11/14/16
  */
 public enum FlightStatus {
-    SCHEDULED('S'),
-    DEPARTED('D'),
-    ARRIVED('A'),
-    CANCELED('C');
+    SCHEDULED("Scheduled", 'S'),
+    DEPARTED("Departed", 'D'),
+    ARRIVED("Arrived", 'A'),
+    CANCELED("Canceled", 'C');
 
+    private String description;
     private char statusChar;
 
-    FlightStatus(char statusChar) {
+    FlightStatus(String desc, char statusChar) {
+        this.description = desc;
         this.statusChar = statusChar;
+    }
+
+    public String getDesc() {
+        return description;
     }
 
     public char getStatusChar() {
         return statusChar;
     }
+
 }
--- src/xyz/jameskr/fm/schedule/enums/FlightType.java
+++ src/xyz/jameskr/fm/schedule/enums/FlightType.java
@@ -1,17 +1,25 @@
 package xyz.jameskr.fm.schedule.enums;
 
 /**
+ * Enum to easy interface with flight type required datatype of char.
+ *
  * @author James 
  * @date 11/14/16
  */
 public enum FlightType {
-    DOMESTIC('D'),
-    INTERNATIONAL('I');
+    DOMESTIC("Domestic", 'D'),
+    INTERNATIONAL("International", 'I');
 
+    private String description;
     private char type;
 
-    FlightType(char type) {
+    FlightType(String desc, char type) {
+        this.description = desc;
         this.type = type;
+    }
+
+    public String getDesc() {
+        return description;
     }
 
     public char getType() {
